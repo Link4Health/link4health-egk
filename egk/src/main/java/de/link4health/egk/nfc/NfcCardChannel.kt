f1b26df3fe5b8fb16fa29c7e7efd5edf4201c89a
Normen Düring <normen.duering@ehealthexperts.de>
2024-08-12 18:14:41 +0200

    Add NfcCardChannel class for NFC card communication
    Introduced the NfcCardChannel class implementing ICardChannel and Closeable interfaces. This class enables transmitting command APDUs and handles the card communication, including resource management by closing connections.

---
+++ egk/src/main/java/de/link4health/egk/nfc/NfcCardChannel.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2024 gematik GmbH
+ * 
+ * Licensed under the EUPL, Version 1.2 or – as soon they will be approved by
+ * the European Commission - subsequent versions of the EUPL (the Licence);
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ * 
+ *     https://joinup.ec.europa.eu/software/page/eupl
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ * 
+ */
+
+package de.link4health.egk.nfc
+
+import de.link4health.egk.card.ICardChannel
+import de.link4health.egk.command.CommandApdu
+import de.link4health.egk.command.ResponseApdu
+import java.io.Closeable
+
+class NfcCardChannel internal constructor(
+    override val isExtendedLengthSupported: Boolean,
+    private val nfcHealthCard: NfcHealthCard
+) : ICardChannel, Closeable {
+    override val card: NfcHealthCard
+        get() = nfcHealthCard
+
+    override val maxTransceiveLength = card.isoDep.maxTransceiveLength
+
+    /**
+     * Returns the responseApdu after transmitting a commandApdu
+     */
+    override fun transmit(command: CommandApdu): ResponseApdu =
+        nfcHealthCard.transmit(command)
+
+    override fun close() {
+        card.isoDep.close()
+    }
+}
