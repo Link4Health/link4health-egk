d5fdc2d466de6de643347614e6151541b6f09f8b
Normen Düring <normen.duering@ehealthexperts.de>
2024-08-12 18:16:56 +0200

    Add NfcCardSecureChannel.kt for secure NFC communication
    Introduce the NfcCardSecureChannel class, implementing secure messaging capabilities for NFC health card communication. This class utilizes encryption and decryption mechanisms to securely transmit command and response APDUs.

---
+++ egk/src/main/java/de/link4health/egk/nfc/NfcCardSecureChannel.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2024 gematik GmbH
+ * 
+ * Licensed under the EUPL, Version 1.2 or – as soon they will be approved by
+ * the European Commission - subsequent versions of the EUPL (the Licence);
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ * 
+ *     https://joinup.ec.europa.eu/software/page/eupl
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ * 
+ */
+
+package de.link4health.egk.nfc
+
+import de.link4health.egk.card.ICardChannel
+import de.link4health.egk.card.PaceKey
+import de.link4health.egk.card.SecureMessaging
+import de.link4health.egk.command.CommandApdu
+import de.link4health.egk.command.ResponseApdu
+import io.github.aakira.napier.Napier
+
+class NfcCardSecureChannel internal constructor(
+    override val isExtendedLengthSupported: Boolean,
+    private val nfcHealthCard: NfcHealthCard,
+    paceKey: PaceKey
+) : ICardChannel {
+    private var secureMessaging = SecureMessaging(paceKey)
+
+    override val card: NfcHealthCard get() = nfcHealthCard
+
+    override val maxTransceiveLength = card.isoDep.maxTransceiveLength
+
+    /**
+     * Returns the responseApdu after transmitting a commandApdu
+     */
+    override fun transmit(command: CommandApdu): ResponseApdu {
+        Napier.d("Encrypt ----")
+        return secureMessaging.encrypt(command).let { encryptedCommand ->
+            Napier.d("encrypted ----")
+            nfcHealthCard.transmit(encryptedCommand).let { encryptedResponse ->
+                Napier.d("Decrypt ----")
+                secureMessaging.decrypt(encryptedResponse)
+            }
+        }
+    }
+}
