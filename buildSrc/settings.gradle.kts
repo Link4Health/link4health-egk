ded314d6e29fff8ceee92be1735a7005a12f0692
Sebastian Melchior <801781+smelchior@users.noreply.github.com>
2026-07-08 10:17:51 +0200

    feat: add artifact registry deployment

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -2,64 +2,18 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
     repositories {
-        val contextUrl = "https://nexus.link4.health/repository"
-        val nexusUsername: String? by extra
-        val nexusPassword: String? by extra
-        val anonymous: String? by extra
-        val anonymousAccess = anonymous?.toBoolean() ?: true
-        if (anonymousAccess) {
-            println("using anonymous access")
-            maven {
-                name = "Link4Health Nexus"
-                url = uri("$contextUrl/link4health-anonymous/")
-                mavenContent {
-                    releasesOnly()
-                }
-            }
-        } else {
-            println("using login access")
-            maven {
-                name = "Link4Health Nexus"
-                url = uri("$contextUrl/link4health-development/")
-                credentials {
-                    username = nexusUsername
-                    password = nexusPassword
-                }
-            }
-        }
+        google()
+        mavenCentral()
+        gradlePluginPortal()
     }
 }
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        val contextUrl = "https://nexus.link4.health/repository"
-        val nexusUsername: String? by extra
-        val nexusPassword: String? by extra
-        val anonymous: String? by extra
-        val anonymousAccess = anonymous?.toBoolean() ?: true
-        if (anonymousAccess) {
-            println("using anonymous access")
-            mavenLocal()
-            maven {
-                name = "Link4Health Nexus"
-                url = uri("$contextUrl/link4health-anonymous/")
-                mavenContent {
-                    releasesOnly()
-                }
-            }
-        } else {
-            println("using login access")
-            mavenLocal()
-            maven {
-                name = "Link4Health Nexus"
-                url = uri("$contextUrl/link4health-development/")
-                credentials {
-                    username = nexusUsername
-                    password = nexusPassword
-                }
-            }
-        }
+        mavenLocal()
+        google()
+        mavenCentral()
     }
     versionCatalogs {
         create("libs") {
